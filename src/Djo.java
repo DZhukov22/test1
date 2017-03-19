1d2a2c3aa129793d73beecd24ee7affde5a67349
TWILIGH-UUEN4FJ\Admin <dscjncrbq@>
2017-03-19 15:18:52 +0300

    первый коммит

---
+++ src/Djo.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Admin on 17.09.2016.
+ *
+ * Hello world !
+ *
+ *
+ * РЕШЕНИЕ:
+ * создадим в папке src - файл с нашим классом (этот файл)
+ * создадим метод  public static void main(String[] args)
+ */
+public class Djo {
+
+    public static void main(String[] args){
+
+        System.out.println("Привет, мир");
+
+    }
+}
